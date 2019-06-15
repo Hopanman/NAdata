5f01434d890562f1d7f429c612b85d9f8e0bc525
Hopanman <soredoom9@naver.com>
2019-06-15 14:44:53 +0900

    파일정리

--- KMDB 관련/KMDB변수크롤링.py
+++ KMDB 관련/KMDB변수크롤링.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from urllib.parse import quote
 import urllib.request
 import json
@@ -11,14 +5,7 @@ from pandas import DataFrame
 from json import JSONDecodeError
 import re
 
-
-# In[2]:
-
-
-service_key = '7J3BA5CHYBGW4H91UV78'
-
-
-# In[3]:
+service_key = ''
 
 
 def urlRequest(url):
@@ -34,10 +21,6 @@ def urlRequest(url):
         print('크롤링 실패 ㅜㅜ', e, '확인바람')
         
         return None
-
-
-# In[4]:
-
 
 def movieExtractor(startdate, enddate, count):
     
@@ -67,10 +50,6 @@ def movieExtractor(startdate, enddate, count):
             print(e)
             print('JSON데이터에 문제가 있습니다 직접 확인해주세요!')
             return None
-
-
-# In[5]:
-
 
 def kmdbValueSearcher(year, index, *keys):
     
@@ -102,37 +81,11 @@ def kmdbValueSearcher(year, index, *keys):
         return valuelist
 
 
-# In[6]:
-
-
 yearlist = ['2010','2011','2012','2013','2014','2015','2016','2017','2018','2019']
-
-
-# In[7]:
-
-
 startdate = '0101'
-
-
-# In[8]:
-
-
 enddate = '0609'
-
-
-# In[9]:
-
-
 movielist=list()
-
-
-# In[10]:
-
-
 columns = ['개봉연도','영화제목','키워드','수상내역']
-
-
-# In[11]:
 
 
 for year in yearlist:
@@ -161,13 +114,7 @@ for year in yearlist:
             print(e)
 
 
-# In[12]:
-
-
 errorlist = ['2015년123','2015년362','2015년741','2016년515','2017년838','2018년432','2018년653']
-
-
-# In[13]:
 
 
 for error in errorlist:
@@ -176,38 +123,10 @@ for error in errorlist:
     movielist.append([int(intargs[0]), valuelist[0], valuelist[1], valuelist[2]+'|'+valuelist[3]])
 
 
-# In[14]:
-
-
 movietable = DataFrame(movielist, columns=columns)
-
-
-# In[15]:
-
-
 movietable = movietable.sort_values('영화제목')
-
-
-# In[16]:
-
-
 movietable['영화제목'] = movietable['영화제목'].apply(lambda x: x.strip())
-
-
-# In[17]:
-
-
 movietable['수상내역'] = movietable['수상내역'].apply(lambda x: x.strip('|'))
-
-
-# In[18]:
-
-
 movietable = movietable.reindex(['영화제목','개봉연도','키워드','수상내역'], axis=1)
-
-
-# In[19]:
-
-
 movietable.to_csv('KMDB변수.csv', index=False, encoding='utf-8')
 
