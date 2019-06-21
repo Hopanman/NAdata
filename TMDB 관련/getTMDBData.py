5ec316007e93c52aad07e6d99c0a6290fb4a5ff9
kjsgh <sawnyu@naver.com>
2019-06-21 18:17:18 +0900

    김지수 TMDB 코드 수정

--- TMDB 관련/getTMDBData.py
+++ TMDB 관련/getTMDBData.py
@@ -1,5 +1,6 @@
-import http.client
+﻿import http.client
 import json
+import pandas as pd
 
 
 payload = "{}"
@@ -7,7 +8,10 @@ TMDBData = []
 
 # page 맥시멈 : 1000
 # 개봉일자가 신뢰도가 떨어진 영화가 많아서 현재까지 한국에서 개봉한 모든 영화를 크롤링하는 코드를 작성했습니다.
-for idx in range(1000):
+# for idx in range(1000):
+for idx in range(1):
+# for idx in range(200):
+# for idx in range(200, 425):   
     mainUrl = "/3/discover/movie?page="
     mainUrl += str(idx+1) 
     mainUrl += "&include_video=true&include_adult=true&sort_by=original_title.asc&language=ko-KR&region=KR&api_key=2b4a725a283b9fa435aca3507c61bae2"
@@ -17,11 +21,11 @@ for idx in range(1000):
         conn.request("GET", mainUrl, payload)
         main_res = conn.getresponse()
         if main_res.getcode() == 200:
-            print('Connected!')
+            print('[mainUrl] Connected!')
             json_main = json.loads(main_res.read().decode('utf-8'))
        
     except Exception as e:
-        print('Access failed.', e)
+        print('[mainUrl] Access failed.', e)
         continue
 
     movieList = json_main['results']
@@ -30,27 +34,38 @@ for idx in range(1000):
  
     for onemovie in movieList:
 #         print(onemovie)
-# #         print(onemovie.keys())
+#         print('-'*50)
+#         print(onemovie.keys())
         id = onemovie['id']
-        title = onemovie['title']
-#         print(id)
-#         print(title)
-#         print(ori_lang)
-# #         print(type(id))
+        movieNm = onemovie['title']
+        openDt = onemovie['release_date']
+        if openDt == None:
+            openDt = ''
+
         detailUrl = "/3/movie/"
         detailUrl += str(id)
         detailUrl += "?language=en-US&api_key=2b4a725a283b9fa435aca3507c61bae2"
-          
-        conn = http.client.HTTPSConnection("api.themoviedb.org")
-        conn.request("GET", detailUrl, payload)
-  
-        detail_res = conn.getresponse()
-        detail_data = detail_res.read()
-        decoded_detail = detail_data.decode("utf-8")
-        json_detail = json.loads(decoded_detail)
-#         print(json_detail)
+        
+        try:
+            conn = http.client.HTTPSConnection("api.themoviedb.org")
+            conn.request("GET", detailUrl, payload)
+            detail_res = conn.getresponse()
+            if detail_res.getcode() == 200:
+                print('[detailUrl] Connected!')
+                json_detail = json.loads(detail_res.read().decode('utf-8'))
+       
+        except Exception as e:
+            print('[detailUrl] Access failed.', e)
+            continue
+
+# #         print(json_detail)
 # #         print(type(json_detail))
+        movieOriNm = json_detail['title']
+#         print(movieOriNm)
+#         print('-'*50)
         budget = json_detail['budget']
+        if budget == 0:
+            budget = ''
         series_dict = json_detail['belongs_to_collection']
         if series_dict != None:
             series = 1
@@ -59,26 +74,35 @@ for idx in range(1000):
         ori_lang_list = json_detail['spoken_languages']
 #         print(ori_lang_list)
         ori_lang = []
-        for one_lang in ori_lang_list:
-            ori_lang.append( one_lang['iso_639_1'] )
+        if ori_lang_list != []:
+            for onelang in ori_lang_list :
+                ori_lang.append( onelang['iso_639_1'] )
+        else : 
+            ori_lang = ''
+        
 #         print(budget) 
 #         print(series)
 #         print(ori_lang)
-               
+
         creditUrl = "/3/movie/"
         creditUrl += str(id)
         creditUrl += "/credits?api_key=2b4a725a283b9fa435aca3507c61bae2"
+        
+        try:
+            conn = http.client.HTTPSConnection("api.themoviedb.org")
+            conn.request("GET", creditUrl, payload)
+            credit_res = conn.getresponse()
+            if credit_res.getcode() == 200:
+                print('[creditUrl] Connected!')
+                json_credit = json.loads(credit_res.read().decode('utf-8'))
+       
+        except Exception as e:
+            print('[creditUrl] Access failed.', e)
+            continue
          
-        conn = http.client.HTTPSConnection("api.themoviedb.org")
-        conn.request("GET", creditUrl, payload)
-         
-        credit_res = conn.getresponse()
-        credit_data = credit_res.read()
-        decoded_credit = credit_data.decode('utf-8')
-        json_detail = json.loads(decoded_credit)
-#         print(json_detail)
-        crew_list = json_detail['crew']
-        cast_list = json_detail['cast']
+#         print(json_credit)
+        crew_list = json_credit['crew']
+        cast_list = json_credit['cast']
 #         print(crew_list)
 #         print('-'*20+'crew'+'-'*20)
 #         print(cast_list)
@@ -87,31 +111,88 @@ for idx in range(1000):
     
         crewData = []
         castData = []
-        for onecrew in crew_list :
-            crewDict = dict()
-            crewDict['name'] = onecrew['name']
-            crewDict['department'] = onecrew['department']
-            crewDict['gender'] = onecrew['gender']
-            crewData.append(crewDict)
-               
-        for onecast in cast_list :
-            castDict = dict()
-            castDict['name'] = onecast['name']
-            castDict['gender'] = onecast['gender']
-            castData.append(castDict)
+        movieDr = []
+        
+        if crew_list != []:
+            for onecrew in crew_list :
+                crewDict = dict()
+                crewDict['name'] = onecrew['name']
+                crewDict['department'] = onecrew['job']
+                crewDict['gender'] = onecrew['gender']
+                crewData.append(crewDict)
+                if onecrew['job'] == 'Director':
+                    movieDr.append(onecrew['name'])
+        else : 
+            crewData = ''
+            movieDr = ''
+        
+        if cast_list != []:
+            for onecast in cast_list :
+                castDict = dict()
+                castDict['name'] = onecast['name']
+                castDict['gender'] = onecast['gender']
+                castData.append(castDict)
+        else : 
+            castData = ''
         
 #         print(crewData)
 #         print('-'*20+'crew'+'-'*20)
 #         print(castData)
 #         print('-'*20+'cast'+'-'*20)
- 
-        TMDBData.append([title, budget, ori_lang, series, crewData, castData])
-#     print(movieData)
-#     print('-'*50)
-#     print('#'*50)
 
-print(TMDBData)
+        
+        keyUrl = "/3/movie/"
+        keyUrl += str(id)
+        keyUrl += "/keywords?api_key=2b4a725a283b9fa435aca3507c61bae2"
+        try:
+            conn = http.client.HTTPSConnection("api.themoviedb.org")
+            conn.request("GET", keyUrl, payload)
+            key_res = conn.getresponse()
+            if key_res.getcode() == 200:
+                print('[keyUrl] Connected!')
+                json_key = json.loads(key_res.read().decode('utf-8'))
+       
+        except Exception as e:
+            print('[keyUrl] Access failed.', e)
+            continue
+            
+        key_list = json_key['keywords']
+        
+#         print(key_list)
+
+        keyWord = []
+        if key_list != []:
+            for onekey in key_list:
+                keyWord.append( onekey['name'])
+        else:
+            keyWord = ''
+                
+
+            
+#         print(movieNm)
+#         print('key:',keyword)
+#         print('-'*50)
+
+        TMDBData.append([movieNm, movieOriNm, openDt, movieDr, budget, ori_lang, series, crewData, castData, keyWord])
+# # #     print(TMDBData)
+# # #     print('-'*50)
+# # #     print('#'*50)
+        print('-'*50)
+    print('#'*50)
+
+
+# print(TMDBData)
+
+TMDBDf = pd.DataFrame(TMDBData, columns=['movieNm', 'movieOriNm', 'openDt', 'movieDr', 'budget', 'ori_lang', 'series', 'crewData', 'castData', 'keyWord'])
+
+print(TMDBDf)
+print('-'*50)
+
+TMDBDf.to_csv('TMDB(1)_oriNm.csv', header=True, index = False, encoding='utf-8')
+# TMDBDf.to_csv('TMDB(2)_oriNm.csv', header=True, index = False, encoding='utf-8')
+
 print('finished')
     
 
-### gender - 1: 여자 / 2: 남자 / 0 : 결측?
+# ### gender - 1: 여자 / 2: 남자 / 0 : 결측?
+# ### 시리즈 - 0: 없음 / 1: 있음
