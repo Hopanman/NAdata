f0eed67fdab366f57e3638dd58e8fd398d7884dc
Hopanman <soredoom9@naver.com>
2019-07-02 14:04:12 +0900

    폴더정리 및 테이블분리

--- Youtube 관련/유튜브크롤링.R
+++
@@ -1,9 +0,0 @@
-install.packages('tuber')
-install.packages('tidyverse')
-library(tuber)
-library(tidyverse)
-
-yt_oauth('', '')
-
-
-get_stats('spQtwggaCy4')
---
+++ 데이터 수집 관련/Youtube 관련/유튜브크롤링.R
@@ -0,0 +1,9 @@
+install.packages('tuber')
+install.packages('tidyverse')
+library(tuber)
+library(tidyverse)
+
+yt_oauth('', '')
+
+
+get_stats('spQtwggaCy4')
