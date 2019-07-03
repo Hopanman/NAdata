84dfd8bfe2d4f6710480cc3ea0ccb83e6b791b47
Hopanman <soredoom9@naver.com>
2019-07-03 17:10:48 +0900

    hojin movie analysis

--- 데이터분석/정호진 데이터 분석/movie_analysis/movie.R
+++ 데이터분석/정호진 데이터 분석/movie_analysis/movie.R
@@ -81,3 +81,55 @@ glimpse(movie)
 sum(is.na(movie$SHOW_TM))
 ggplot(movie,aes(x=SHOW_TM, y=AUDI_ACC, color=SHOW_TM)) + geom_point() + scale_color_viridis(begin = 1, end = 0, option = 'D') + geom_smooth(method = 'lm', color = 'red3', fill = 'red3') + scale_y_continuous(breaks = c(0, 5000000, 10000000, 15000000), labels = c('0명', '오백만명', '천만명', '천오백만명')) + scale_x_continuous(breaks=c(60,120,180,240), labels=c('1시간','2시간','3시간','4시간')) + theme_classic()  + labs(title='상영시간에 따른 관객수', x='상영시간', y='관객수') + theme(plot.title=element_text(hjust=0.5, face='bold'), legend.position = 'none')
 cor(movie$SHOW_TM, movie$AUDI_ACC, use='complete.obs')
+
+
+
+
+
+
+glimpse(movie)
+sum(is.na(movie$BUDGET))
+ggplot(filter(movie, !is.na(BUDGET)),aes(x=BUDGET, y=AUDI_ACC, color=BUDGET)) + geom_point() + scale_color_viridis(begin = 1, end = 0, option = 'D') + geom_smooth(method = 'lm', color = 'red3', fill = 'red3') + scale_y_continuous(breaks = c(0, 5000000, 10000000, 15000000), labels = c('0명', '오백만명', '천만명', '천오백만명')) + scale_x_continuous(breaks=c(0,100000000,200000000,300000000,400000000, 500000000), labels=c('0','1억','2억','3억','4억','5억')) + theme_classic()  + labs(title='예산에 따른 관객수', x='예산(Dollar)', y='관객수') + theme(plot.title=element_text(hjust=0.5, face='bold'), legend.position = 'none')
+cor(movie$BUDGET, movie$AUDI_ACC, use='complete.obs')
+
+
+
+
+
+
+glimpse(movie)
+sum(is.na(movie$NAVER_CMT_NN))
+ggplot(filter(movie, !is.na(NAVER_CMT_NN)),aes(x=NAVER_CMT_NN, y=AUDI_ACC, color=NAVER_CMT_NN)) + geom_point() + scale_color_viridis(begin = 1, end = 0, option = 'D') + geom_smooth(method = 'lm', color = 'red3', fill = 'red3') + scale_y_continuous(breaks = c(0, 5000000, 10000000, 15000000, 20000000), labels = c('0명', '오백만명', '천만명', '천오백만명', '이천만명')) + scale_x_continuous(breaks=c(0, 5000, 10000, 15000, 20000, 25000), labels=c('0건','5000건','10000건','15000건','20000건','25000건'))+ theme_classic()  + labs(title='개봉전 네이버 댓글수에 따른 관객수', x='네이버 댓글수', y='관객수') + theme(plot.title=element_text(hjust=0.5, face='bold'), legend.position = 'none')
+cor(movie$NAVER_CMT_NN, movie$AUDI_ACC, use='complete.obs')
+
+
+
+
+
+glimpse(movie)
+ggplot(filter(movie, !is.na(NAVER_PRE_EVAL)),aes(x=NAVER_PRE_EVAL, y=AUDI_ACC, color=NAVER_PRE_EVAL)) + geom_point() + scale_color_viridis(begin = 1, end = 0, option = 'D') + geom_smooth(method = 'lm', color = 'red3', fill = 'red3') + scale_y_continuous(breaks = c(0, 5000000, 10000000, 15000000), labels = c('0명', '오백만명', '천만명', '천오백만명')) + scale_x_continuous(breaks=c(0, 2.5, 5.0, 7.5, 10.0), labels=c('0점','2.5점','5점','7.5점','10점'))+ theme_classic() + labs(title='개봉전 네이버 평점에 따른 관객수', x='네이버 평점', y='관객수') + theme(plot.title=element_text(hjust=0.5, face='bold'), legend.position = 'none')
+cor(movie$NAVER_PRE_EVAL, movie$AUDI_ACC, use='complete.obs')
+
+
+
+
+
+ggplot(filter(movie, !is.na(NAVER_PRE_EVAL_MUL)),aes(x=NAVER_PRE_EVAL_MUL, y=AUDI_ACC, color=NAVER_PRE_EVAL_MUL)) + geom_point() + scale_color_viridis(begin = 1, end = 0, option = 'D') + geom_smooth(method = 'lm', color = 'red3', fill = 'red3') + scale_y_continuous(breaks = c(0, 5000000, 10000000, 15000000, 20000000), labels = c('0명', '오백만명', '천만명', '천오백만명','이천만명'))+ theme_classic() + labs(title='개봉전 네이버 평점x네이버 댓글수에 따른 관객수', x='네이버 평점x네이버 댓글수', y='관객수') + theme(plot.title=element_text(hjust=0.5, face='bold'), legend.position = 'none')
+cor(movie$NAVER_PRE_EVAL_MUL, movie$AUDI_ACC, use='complete.obs')
+
+
+
+
+glimpse(movie)
+ggplot(filter(movie, !is.na(NAVER_EX_PT)),aes(x=NAVER_EX_PT, y=AUDI_ACC, color=NAVER_EX_PT)) + geom_point() + scale_color_viridis(begin = 1, end = 0, option = 'D') + geom_smooth(method = 'lm', color = 'red3', fill = 'red3') + scale_y_continuous(breaks = c(0, 5000000, 10000000, 15000000), labels = c('0명', '오백만명', '천만명', '천오백만명')) + theme_classic() + labs(title='개봉전 네이버 기대지수에 따른 관객수', x='기대지수', y='관객수') + theme(plot.title=element_text(hjust=0.5, face='bold'), legend.position = 'none')
+ggplot(filter(movie, !is.na(NAVER_EX_PT)),aes(x=log1p(NAVER_EX_PT), y=AUDI_ACC, color=log1p(NAVER_EX_PT))) + geom_point() + scale_color_viridis(begin = 1, end = 0, option = 'D') + geom_smooth(method = 'lm', color = 'red3', fill = 'red3') + scale_y_continuous(breaks = c(0, 5000000, 10000000, 15000000), labels = c('0명', '오백만명', '천만명', '천오백만명')) + theme_classic() + labs(title='개봉전 네이버 기대지수(로그)에 따른 관객수', x='log(1+기대지수)', y='관객수') + theme(plot.title=element_text(hjust=0.5, face='bold'), legend.position = 'none')
+cor(movie$NAVER_EX_PT, movie$AUDI_ACC, use='complete.obs')
+
+
+
+
+ggplot(movie, aes(x=AUDI_ACC)) + geom_histogram()
+
+geom_histogram
+
+max(log(movie$AUDI_ACC))
