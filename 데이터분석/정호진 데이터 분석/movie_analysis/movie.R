ef1349e77e2890c7614f67b9fc78b726cbce56ba
Hopanman <soredoom9@naver.com>
2019-07-04 18:36:45 +0900

    20190704논의내용

--- 데이터분석/정호진 데이터 분석/movie_analysis/movie.R
+++ 데이터분석/정호진 데이터 분석/movie_analysis/movie.R
@@ -7,6 +7,7 @@ library(gridExtra)
 library(VIM)
 library(lubridate)
 library(randomForest)
+library(lawstat)
 
 getwd()
 movie <- read.csv('movie_regression.csv', na.strings = c(''))
@@ -129,7 +130,91 @@ cor(movie$NAVER_EX_PT, movie$AUDI_ACC, use='complete.obs')
 
 
 ggplot(movie, aes(x=AUDI_ACC)) + geom_histogram()
-
 geom_histogram
-
 max(log(movie$AUDI_ACC))
+
+glimpse(movie)
+
+movie$OPEN_YEAR <- factor(movie$OPEN_YEAR)
+movie$OPEN_MONTH <- factor(movie$OPEN_MONTH)
+movie$OPEN_DAY <- factor(movie$OPEN_DAY)
+movie$OPEN_WEEKDAY <- factor(movie$OPEN_WEEKDAY)
+movie$OPEN_QUARTER <- factor(movie$OPEN_QUARTER)
+movie$OPEN_WEEK <- factor(movie$OPEN_WEEK)
+
+
+
+# summary(aov(AUDI_ACC~TITLE, data = movie))
+
+# 데이터의 정규분포 여부 확인
+shapiro.test(movie$AUDI_ACC)
+
+
+#두 집단간 차이 분석
+str(movie)
+wilcox.test(AUDI_ACC~SERIES,data=movie)
+glimpse(movie)
+movie$AWARDS <- factor(movie$AWARDS)
+wilcox.test(AUDI_ACC~AWARDS,data=movie)
+
+
+#세 집단이상간 차이 분석
+levene.test(movie$AUDI_ACC, movie$OPEN_YEAR, location='mean')
+summary(aov(AUDI_ACC~OPEN_YEAR, data=movie))
+kruskal.test(AUDI_ACC~NATION_NM_NUM, data=movie)
+
+
+table(movie$ORI_BOOK)
+
+movie %>% group_by(ACTOR2) %>% summarise(actor_n = n(), audi_median=median(AUDI_ACC), audi_mean=mean(AUDI_ACC)) %>% arrange(desc(actor_n)) %>% filter(!is.na(ACTOR2)) %>% head(20) 
+
+
+ggplot(top20_actor, aes(x=ACTOR1,y=audi_median,fill=ACTOR1)) + geom_bar(stat='identity') + scale_fill_viridis(discrete = T, option = 'E', begin = 1, end = 0) + scale_y_continuous(breaks=c(0,1000000,2000000,3000000,4000000,5000000), labels=c('0','백만명','이백만명','삼백만명','사백만명','오백만명'))  + theme_classic() + labs(title='주인공출연빈도 top20배우의 관객수 중앙값', x='배우', y='관객수 중앙값') + theme(plot.title=element_text(hjust=0.5, face='bold'), legend.position = 'none', axis.text.x = element_text(angle=90))
+
+
+
+
+
+glimpse(movie)
+movie %>% group_by(COMPANY_NM) %>% summarise(cnt = n(), audi_median=median(AUDI_ACC), audi_mean=mean(AUDI_ACC)) %>% arrange(desc(cnt)) %>% filter(!is.na(COMPANY_NM)) %>% head(20)
+ggplot(top10_company, aes(x=COMPANY_NM,y=audi_median,fill=COMPANY_NM)) + geom_bar(stat='identity') + scale_fill_viridis(discrete = T, option = 'E', begin = 1, end = 0) + scale_y_continuous(breaks=c(0,200000,400000,600000,800000), labels=c('0','20만명','40만명','60만명','80만명'))  + theme_classic() + labs(title='빈도수 top10배급사의 관객수 중앙값', x='배급사', y='관객수 중앙값') + theme(plot.title=element_text(hjust=0.5, face='bold'), legend.position = 'none') + coord_flip()
+
+
+table(movie$PRI_GENRE_NM)
+glimpse(movie)
+movie %>% group_by(PRI_GENRE_NM) %>% summarise(cnt = n(), audi_median=median(AUDI_ACC), audi_mean=mean(AUDI_ACC)) %>% arrange(desc(cnt)) %>% filter(!is.na(PRI_GENRE_NM)) %>% head(20)
+ggplot(filter(movie, !is.na(PRI_GENRE_NM)), aes(x=PRI_GENRE_NM, y=log(AUDI_ACC), fill=PRI_GENRE_NM)) + stat_summary_bin(fun.y = mean, geom='bar') + scale_fill_viridis(discrete = T, option = 'E', begin = 1, end = 0) + coord_flip() + theme_classic() + labs(title='장르에 따른 관객수(로그) 평균', x='장르', y='log(관객수) 평균') + theme(plot.title=element_text(hjust=0.5, face='bold'), legend.position = 'none')
+
+
+
+
+
+
+
+movie %>% group_by(NATION_NM) %>% summarise(cnt = n(), audi_median=median(AUDI_ACC), audi_mean=mean(AUDI_ACC)) %>% arrange(desc(cnt)) %>% filter(!is.na(NATION_NM)) %>% head(20)
+ggplot(top20_nation, aes(x=NATION_NM,y=audi_median,fill=NATION_NM)) + geom_bar(stat='identity') + scale_fill_viridis(discrete = T, option = 'E', begin = 1, end = 0)  + coord_flip() + scale_y_continuous(breaks=c(0,20000,40000,60000), labels=c('0','2만명','4만명','6만명'))  + theme_classic() + labs(title='빈도수 top20제작국가의 관객수 중앙값', x='제작국가', y='관객수 중앙값') + theme(plot.title=element_text(hjust=0.5, face='bold'), legend.position = 'none')
+
+
+
+
+table(movie$SP_LANG_NUM)
+top20_lang <- movie %>% group_by(SP_LANG) %>% summarise(cnt = n(), audi_median=median(AUDI_ACC), audi_mean=mean(AUDI_ACC)) %>% arrange(desc(cnt)) %>% filter(!is.na(SP_LANG)) %>% head(20)
+ggplot(top20_lang, aes(x=SP_LANG,y=audi_median,fill=SP_LANG)) + geom_bar(stat='identity') + scale_fill_viridis(discrete = T, option = 'E', begin = 1, end = 0) + coord_flip() + scale_y_continuous(breaks=c(0,200000,400000), labels=c('0','20만명','40만명')) + theme_classic() + labs(title='top20사용언어에 따른 관객수 중앙값', x='사용언어', y='관객수 중앙값') + theme(plot.title=element_text(hjust=0.5, face='bold'), legend.position = 'none')
+
+top20_lang
+movie$ACTOR1
+
+glimpse(movie)
+ggplot(filter(movie, !is.na(AWARDS)), aes(x=AWARDS, y=AUDI_ACC, fill=AWARDS)) + stat_summary_bin(fun.y = median, geom='bar') + scale_fill_viridis(discrete = T, option = 'E', begin = 1, end = 0) + scale_y_continuous(breaks=c(0,10000,20000), labels=c('0','만명','2만명')) + scale_x_discrete(breaks=c(0,1), labels=c('비수상','수상')) + theme_classic() + labs(title='개봉전 수상여부에 따른 관객수 중앙값', x='수상여부', y='관객수 중앙값') + theme(plot.title=element_text(hjust=0.5, face='bold'), legend.position = 'none')
+
+
+table(movie$PRI_GENRE_NM)
+
+movie$NATION_NM_NUM <- factor(movie$NATION_NM_NUM)
+movie$COMPANY_NM_NUM <- factor(movie$COMPANY_NM_NUM)
+movie$GENRE_NM_NUM <- factor(movie$GENRE_NM_NUM)
+movie$SP_LANG_NUM <- factor(movie$SP_LANG_NUM)
+glimpse(movie)
+
+
+
