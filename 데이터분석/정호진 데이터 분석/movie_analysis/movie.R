b8ba6c04cd50696c4e2e2679d97acb1465698e0a
Hopanman <soredoom9@naver.com>
2019-07-05 18:35:57 +0900

    1차회귀결과

--- 데이터분석/정호진 데이터 분석/movie_analysis/movie.R
+++ 데이터분석/정호진 데이터 분석/movie_analysis/movie.R
@@ -9,6 +9,7 @@ library(lubridate)
 library(randomForest)
 library(lawstat)
 library(varhandle)
+library(caTools)
 
 
 getwd()
@@ -322,3 +323,38 @@ write.csv(train,'train.csv',row.names = F, fileEncoding = 'cp949')
 train <- train[-c(1,2)]
 
 write.csv(train,'train_regression.csv',row.names = F, fileEncoding = 'cp949')
+
+
+#훈련 전 전처리
+train <- read.csv('train_regression.csv')
+train$OPEN_MONTH <- factor(train$OPEN_MONTH)
+train$OPEN_QUARTER <- factor(train$OPEN_QUARTER)
+train$SERIES <- factor(train$SERIES)
+train$ORI_BOOK <- factor(train$ORI_BOOK)
+glimpse(train)
+summary(train)
+train <- mutate(train, NAVER_CMT_NN = log10(NAVER_CMT_NN+1), NAVER_EX_PT = log10(NAVER_EX_PT+1), AUDI_ACC = log10(AUDI_ACC))
+
+#학습용 데이터와 검증용 데이터 분리
+sample <- sample.split(train$AUDI_ACC,SplitRatio = 0.80)
+real_train <- subset(train, sample==T)
+test <- subset(train, sample==F)
+
+
+#랜덤포레스트를 이용한 영화데이터 기계학습(선형회귀)
+set.seed(222)
+model <- randomForest(AUDI_ACC ~ .,data = real_train, ntree = 501, replace = TRUE, nodesize = 9,importance = TRUE)
+
+
+model
+#변수중요도와 노드불순도확인
+importance(model)
+
+
+#검증용데이터로 예측
+pred <- predict(model, test[-12])
+test$AUDI_ACC_PRED <- pred
+test <- test[-13]
+test <- mutate(test, AUDI_ACC = 10^AUDI_ACC, AUDI_ACC_PRED = 10^pred)
+write.csv(test,'prediction.csv',row.names = F, fileEncoding = 'cp949')
+
