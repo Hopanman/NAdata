e752fe5bdb51487fbe28f9b665c72760b55e4a4e
Hopanman <soredoom9@naver.com>
2019-07-08 02:07:48 +0900

    정호진예측결과

--- 데이터분석/정호진 데이터 분석/movie_analysis/2차회귀.R
+++ 데이터분석/정호진 데이터 분석/movie_analysis/2차회귀.R
@@ -13,6 +13,8 @@ real_train$SERIES <- factor(real_train$SERIES)
 real_train$ORI_BOOK <- factor(real_train$ORI_BOOK)
 glimpse(real_train)
 
+
+
 #결측치 처리
 aggr(real_train, sortVars=T,prop=F, cex.axis=0.45, numbers=T)
 real_train$SERIES[is.na(real_train$SERIES)] <- 0
@@ -36,9 +38,45 @@ test <- subset(real_train, sample==F)
 
 #랜덤포레스트를 이용한 영화데이터 기계학습(선형회귀)
 set.seed(222)
-model <- randomForest(AUDI_ACC ~ .,data = movie_train, ntree = 501, replace = TRUE, nodesize = 9,importance = TRUE)
+model <- randomForest(AUDI_ACC ~ .,data = real_train, ntree = 501, replace = TRUE, nodesize = 9,importance = TRUE)
 model
 
-
+glimpse(real_train)
 #변수 중요도 확인 
 importance(model)
+
+
+#예측영화 로딩
+test <- read.csv('test.csv',na.strings = c(''))
+
+#필요한 컬럼 추출 
+glimpse(test)
+test <- test[-c(1,2)]
+test <- test[-3]
+glimpse(test)
+
+
+#factor타입 동일조건 설정
+test$AUDI_ACC <- NA
+test
+imsi <- rbind(real_train,test)
+glimpse(imsi)
+test <- imsi[4188:4207,]
+glimpse(test)
+test <- test[-12]
+glimpse(test)
+real_train$PRI_GENRE_NM
+test$PRI_GENRE_NM
+
+
+#예측데이터 정규화
+test <- mutate(test, NAVER_CMT_NN = log10(NAVER_CMT_NN+1), NAVER_EX_PT = log10(NAVER_EX_PT+1))
+summary(test)
+
+#관객수 예측
+y_pred <- predict(model,test)
+test$AUDI_ACC_PRED <- y_pred
+test <- mutate(test, AUDI_ACC_PRED=10^(AUDI_ACC_PRED))
+test
+test$AUDI_ACC_PRED <- floor(test$AUDI_ACC_PRED)
+write.csv(test,'prediction.csv',row.names = F, fileEncoding = 'cp949')
